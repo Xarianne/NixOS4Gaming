9aad13c02437e1253c74c587859d5ec1ea28563e
Xariann <130084241+Xarianne@users.noreply.github.com>
2025-08-16 13:23:55 +0100

    Update emulation.nix

--- modules/gaming/emulation.nix
+++ modules/gaming/emulation.nix
@@ -2,15 +2,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  options = {
-    gaming.emulation.enable = lib.mkEnableOption "retro gaming emulation support";
-  };
-
-  config = lib.mkIf config.gaming.emulation.enable {
-    environment.systemPackages = with pkgs; [
-      retroarch
-      retroarch-assets
-      retroarch-joypad-autoconfig
-    ];
-  };
+  environment.systemPackages = with pkgs; [
+    retroarch
+    retroarch-assets
+    retroarch-joypad-autoconfig
+  ];
 }
