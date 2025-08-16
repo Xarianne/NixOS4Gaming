3af883845501af3ea1e671eed989e942b019d3fc
Xariann <130084241+Xarianne@users.noreply.github.com>
2025-08-16 11:27:57 +0100

    Fix: forgot a #

--- modules/flatpak/flatpaks.nix
+++ modules/flatpak/flatpaks.nix
@@ -10,7 +10,7 @@
     { appId = "com.obsproject.Studio"; origin = "flathub"; }
     "com.obsproject.Studio.Plugin.InputOverlay" # OBS addon
     "com.obsproject.Studio.Plugin.BackgroundRemoval" # OBS addon
-    "org.freedesktop.Platform.VulkanLayer.OBSVkCapture" OBS addon
+    "org.freedesktop.Platform.VulkanLayer.OBSVkCapture" # OBS addon
     "com.vivaldi.Vivaldi"
     "com.discordapp.Discord"
     "org.localsend.localsend_app"
