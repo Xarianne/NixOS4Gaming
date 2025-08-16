d1892731206aa1c1cd2a3616d149a46ad6de4986
Xariann <130084241+Xarianne@users.noreply.github.com>
2025-08-16 12:00:43 +0100

    Feat: Added wheel and joystick support

--- modules/hardware/peripherals.nix
+++ modules/hardware/peripherals.nix
@@ -1,3 +1,4 @@
+# etc/nixos/modules/hardware/peripherals.nix
 { pkgs, ... }:
 {
   # Enable udevrules for OpenRGB
@@ -14,6 +15,19 @@
 
   # Secure boot tools if using lanzaboote
   environment.systemPackages = with pkgs; [
+    
     python313Packages.openrazer
+
+    # Wheel and Joystic testing and configuration tools
+    jstest-gtk          # GUI for testing/calibrating joysticks
+    linuxConsoleTools   # Includes jscal, jstest (command line tools
   ];
+
+  # Optional: Add user to input group (should not be needed on modern NixOS)
+  # users.users.youruser.extraGroups = [ "input" ];
+
+  # Modern kernels should support force feedback and many joysticks 
+  # on their own without the need for third party software
+  # Some distros also add Xone, but kernels nowadays provide good 
+  # controller support out of the box
 }
